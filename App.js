055e9dcde64f1f7ba99406cf7ecc4a9cfa7c4d3e
arbhassan <hassanjawad1321@gmail.com>
2020-12-25 22:28:12 +0500

    Custom Drawer Navigator in ReactNavigation5(1/2)

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import MainTabScreen from './screens/MainTabScreen';
 
+import {DrawerContent} from './screens/DrawerContent';
+
 const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator
+        initialRouteName="Home"
+        drawerContent={(props) => <DrawerContent {...props} />}>
         <Drawer.Screen name="Home" component={MainTabScreen} />
         {/* <Drawer.Screen name="Details" component={DetailsStackScreen} /> */}
       </Drawer.Navigator>
